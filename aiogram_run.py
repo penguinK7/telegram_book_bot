f474156830f29d82e0d952a8ead06b1cf4e699c7
Ksenia Klenkova <kllenkova7@gmail.com>
2025-03-16 19:08:46 +0300

    добавила логи и try в файл aiogram_run.py

--- aiogram_run.py
+++ aiogram_run.py
@@ -1,22 +1,36 @@
 import asyncio
+import logging
 from create_bot import dp, bot, scheduler
 from db_handler.db_class import create_tables
 from handlers.start import start_router
 
 
 async def main():
-    # Создаем таблицы в базе данных
-    create_tables()
+    try:
+        # Создаем таблицы в базе данных
+        create_tables()
+        logging.info("Таблицы успешно созданы.")
 
-    # Подключаем роутер
-    dp.include_router(start_router)
+        # Подключаем роутер
+        dp.include_router(start_router)
+        logging.info("Роутер успешно подключен.")
 
-    # Запускаем планировщик
-    scheduler.start()
+        # Запускаем планировщик
+        scheduler.start()
+        logging.info("Планировщик успешно запущен.")
 
-    # Запускаем бота
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+        # Запускаем бота
+        logging.info("Запускаем бота")
+        await bot.delete_webhook(drop_pending_updates=True)
+        await dp.start_polling(bot)
+
+    except Exception as e:
+        logging.error(f"Произошла ошибка: {e}")
+    finally:
+        logging.info("Выключаем книжного бота")
+        await bot.session.close()
+        scheduler.shutdown()
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
